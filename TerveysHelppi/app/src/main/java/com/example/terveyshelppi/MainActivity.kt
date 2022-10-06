ab4fc48c05285481e3ebcdd1f9d731712941f188
Son Dang <minhson0506@gmail.com>
2022-10-06 21:36:51 +0300

    change endpoint and query for get title from api, add title in youtube screen

--- TerveysHelppi/app/src/main/java/com/example/terveyshelppi/MainActivity.kt
+++ TerveysHelppi/app/src/main/java/com/example/terveyshelppi/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.terveyshelppi
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import com.example.terveyshelppi.Service.YouTubeService.ResultViewModel
-import com.example.terveyshelppi.Service.YouTubeService.SearchResponse
 import com.example.terveyshelppi.Service.YouTubeService.YoutubeScreen
-import com.example.terveyshelppi.Service.YouTubeService.searchOnYoutube
 import com.example.terveyshelppi.ui.theme.TerveysHelppiTheme
 
 class MainActivity : AppCompatActivity() {
@@ -27,7 +20,7 @@ class MainActivity : AppCompatActivity() {
             TerveysHelppiTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting(model)
+                    YoutubeScreen(model)
                 }
             }
         }
@@ -37,25 +30,5 @@ class MainActivity : AppCompatActivity() {
 @ExperimentalFoundationApi
 @Composable
 fun Greeting(model: ResultViewModel) {
-
-    val TAG = "terveyshelppi"
-    var input by remember { mutableStateOf("") }
-    val result: List<SearchResponse.Item>? by model.result.observeAsState(null)
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        Text(text = "Hello Android!")
-        TextField(value = input, onValueChange = { input = it })
-        Button(onClick = {
-            searchOnYoutube(input, model)
-            Log.d(TAG, "Greeting: ${result.toString()}")
-        }) {
-            Text(text = "Search")
-        }
-        if (result != null) {
-            Log.d(TAG, "Greeting: start to load video")
-                YoutubeScreen(videoId = result!!.map { it.id.videoId })
-
-        }
-    }
-
 }
 
